2dad54274b4688c05b709cf88b24e97b275547eb
Wilson <steven@stevenwilson.ca>
2023-03-16 12:41:27 -0230

    Improved error handling

--- calc.py
+++ calc.py
@@ -6,6 +6,12 @@ from prompt_toolkit import print_formatted_text as print
 from prompt_toolkit import PromptSession
 from prompt_toolkit import HTML
 
+class InvalidOperator(Exception):
+    pass
+
+class SimplificationError(Exception):
+    pass
+
 class Calc:
     valid: re.Pattern = re.compile(r'^[0-9-.()+*/\\% ^]+$')
     paren: re.Pattern = re.compile(r'\([^\(\)]+\)')
@@ -39,46 +45,51 @@ class Calc:
         tokens = list(filter(lambda x: x != '', re.split(
             r'([^\d.]{1})', expr.replace(' ', ''))))
 
-        # For each token
-        for k, token in enumerate(tokens):
-            # Ends with operator not allowed
-            if token in Calc.operations and k == len(tokens) - 1:
-                print("Invalid operator use")
-                return self.last_result
-            # Convert negative numbers to negative instead of treating as subtraction
-            # Determined by '-', 'X' at start of expression or immediately following an operator
-            # ['-12', '+', '50', '-', '3', '*', '6']
-            if token == '-' and tokens[k+1] not in Calc.operations and (k == 0 or tokens[k-1] in Calc.operations):
-                tokens[k+1] = token + tokens[k+1]
-                tokens.pop(k)
-                continue
-            # Start with operator/two consecutive operators not allowed
-            if token in Calc.operations and (k == 0 or tokens[k-1] in Calc.operations):
-                print("Invalid operator use")
-                return self.last_result
-            # Prohibit multiple decimal points
-            if token not in Calc.operations and token.count('.') > 1:
-                print("Too many decimal points")
-                return self.last_result
-        
-        # Go through operators by proper order of operations
-        for op in Calc.operations:
-            # While we have a match on this operator
-            while op in tokens:
-                # Token index/position
-                pos = tokens.index(op)
-                # Replace token with result from operation performed on values before and after it
-                tokens[pos] = Calc.operations[tokens[pos]](float(tokens[pos-1]), float(tokens[pos+1]))
-                # Remove value after
-                tokens.pop(pos+1)
-                # Remove value before
-                tokens.pop(pos-1)
+        try:
+            # For each token
+            for k, token in enumerate(tokens):
+                # Ends with operator not allowed
+                if token in Calc.operations and k == len(tokens) - 1:
+                    raise InvalidOperator
+                # Convert negative numbers to negative instead of treating as subtraction
+                # Determined by '-', 'X' at start of expression or immediately following an operator
+                # ['-12', '+', '50', '-', '3', '*', '6']
+                if token == '-' and tokens[k+1] not in Calc.operations and (k == 0 or tokens[k-1] in Calc.operations):
+                    tokens[k+1] = token + tokens[k+1]
+                    tokens.pop(k)
+                    continue
+                # Start with operator/two consecutive operators not allowed
+                if token in Calc.operations and (k == 0 or tokens[k-1] in Calc.operations):
+                    raise InvalidOperator
+                # Prohibit multiple decimal points
+                if token not in Calc.operations and token.count('.') > 1:
+                    raise InvalidOperator
+        except InvalidOperator:
+            print("Invalid operator use")
+            return self.last_result
 
-        # If we're not left with a single value after the process, something went wrong
-        if len(tokens) != 1:
-            raise IndexError
+        try:
+            # Go through operators by proper order of operations
+            for op in Calc.operations:
+                # While we have a match on this operator
+                while op in tokens:
+                    # Token index/position
+                    pos = tokens.index(op)
+                    # Replace token with result from operation performed on values before and after it
+                    tokens[pos] = Calc.operations[tokens[pos]](float(tokens[pos-1]), float(tokens[pos+1]))
+                    # Remove value after
+                    tokens.pop(pos+1)
+                    # Remove value before
+                    tokens.pop(pos-1)
 
-        return float(tokens[0])
+            # If we're not left with a single value after the process, something went wrong
+            if len(tokens) != 1:
+                raise SimplificationError
+
+            return float(tokens[0])
+        except SimplificationError:
+            print("Error occurred while simplifying expression")
+            return self.last_result
 
     def do(self, inp: str) -> None:
         """Parse input"""
